26e6fe81e08d3f871539cd30fa4c50cb5f6fe537
Ksegoviad <kielsegovia16@gmail.com>
2024-05-31 00:17:08 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner leer = new Scanner(System.in);
         System.out.println("Find the #n prime");
-        Scanner prime = new Scanner(System.in);
-        int numPrimes = prime.nextInt();
+
+        int numPrimes = leer.nextInt();
+
         int firstPrime = 2;
-        int i= 1;
+
         do {
+            int i= 1;
             int sum= 0;
             do {
                 if (firstPrime % i == 0) {
                     sum=sum+1;
+
                 }
                 i++;
-            } while (i > firstPrime);
+            } while (i <= firstPrime);
 
             if (sum < 3){
-             System.out.println(firstPrime);
+                System.out.println(firstPrime);
+                numPrimes=numPrimes - 1;
             }
             firstPrime++;
-        } while (i > numPrimes);
-
+        } while (numPrimes > 0);
     }
 }
